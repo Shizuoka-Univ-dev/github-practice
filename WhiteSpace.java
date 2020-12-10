caa048e106418d880bb4f95883834974393201e6
Seele0709 <yu07090618@gmail.com>
2020-12-10 16:00:40 +0900

    修正しました、確認お願いします。

--- WhiteSpace.java
+++ WhiteSpace.java
@@ -1,12 +1,7 @@
-import java.util.Scanner;
-
 class WhiteSpace{
-    static void WhiteSpace(){
-        Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine();
-        String[] r = s.split(" ");
-        for(int i = 0; i < r.length; i++){
-            System.out.print(r[i]);
+    static int WhiteSpace(int s[]){
+        for(int i = 0; i < s.length; i++){
+            System.out.print(s[i] + " ");
         }
     }    
 }
