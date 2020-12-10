773238efb38105524ac44922e3efd0a5ef4a7f5d
Seele0709 <yu07090618@gmail.com>
2020-12-10 15:34:02 +0900

    配列の中身を空白区切りで表示する関数を作りました。

---
+++ WhiteSpace.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class WhiteSpace{
+    static String WhiteSpace(){
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String[] r = s.split(" ");
+        return r[];
+    }    
+}
