08283580ca23903cc72944f7e3252cc942f3c13c
mepooh <me.pooh441@gmail.com>
2020-12-10 15:58:44 +0900

    Stringを返していた箇所を、printするように変更しました

---
+++ hello_n_times.java
@@ -0,0 +1,12 @@
+class hello_n_times{
+    /*
+    public static void main(String args[]){
+        hello_n_times(5);
+    }
+    */
+    static void hello_n_times (int N) {
+        for (int i = 0; i < N; i++){
+            System.out.println("hello");
+        }
+    }
+}
