c336e95366bf294dc537147e2d7c95ea7e67b80d
Seele0709 <yu07090618@gmail.com>
2020-12-10 16:03:29 +0900

    関数をintではなくvoidに直しました。

--- WhiteSpace.java
+++ WhiteSpace.java
@@ -1,5 +1,5 @@
 class WhiteSpace{
-    static int WhiteSpace(int s[]){
+    static void WhiteSpace(int s[]){
         for(int i = 0; i < s.length; i++){
             System.out.print(s[i] + " ");
         }
