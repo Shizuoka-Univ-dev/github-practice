87f4df831546e84b064c29cc19b226167e9de1ac
kaji <kajiharalivi@icloud.com>
2020-12-10 15:21:50 +0900

    add pow

---
+++ pow.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class pow {
+
+    static long _pow(int k, int i) {
+        long n = k;
+        if (i == 0)
+            return 1;
+        if (i == 1)
+            return 1;
+        for (int j = 0; j < i - 1; j++) {
+            n = n * k;
+        }
+        return n;
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        int j = sc.nextInt();
+        long k = _pow(i, j);
+        System.out.println(k);
+    }
+}
