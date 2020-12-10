8ce0f54ae1f407ee218a3cb73969793aa6da172a
mepooh <me.pooh441@gmail.com>
2020-12-10 16:01:28 +0900

    Stringを返していた箇所を、printするように変更しました

--- hello_n_times.java
+++ hello_n_times.java
@@ -1,9 +1,5 @@
 class hello_n_times{
-    /*
-    public static void main(String args[]){
-        hello_n_times(5);
-    }
-    */
+
     static void hello_n_times (int N) {
         for (int i = 0; i < N; i++){
             System.out.println("hello");
